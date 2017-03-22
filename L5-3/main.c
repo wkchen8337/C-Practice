378fdcf20bacc754f7bea274e7a312a2986da74f
allen_chen <wkchen8337@gmail.com>
2017-03-23 02:16:55 +0800

    c-pratice

--- L5-3/main.c
+++ L5-3/main.c
@@ -5,13 +5,15 @@ int check(int v)
 {
     if (v)
     {
-        printf("TRUE");
+        printf("true\n");
     }
 
     else
     {
-        printf("false");
+        printf("false\n");
     }
+
+    return v;
 }
 
 
@@ -20,8 +22,9 @@ int main(void)
     int x = 5;
     int y = 3;
 
-    check(x>2 && y ==3);
-
+    check(x>2 && y ==3);  // true
+    check(!(x <2 || y==3)); //false
+    check(x != 0 &&  y); // true "y«D0"
 
     return 0;
 }
