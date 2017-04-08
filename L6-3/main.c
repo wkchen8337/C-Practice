7f7e2162533e10e10ecc8afe5057c5865cdda4cc
allen_chen <wkchen8337@gmail.com>
2017-04-08 20:55:36 +0800

    C語言練習

--- L6-3/main.c
+++ L6-3/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
+void display(char cr, int lines, int width);
+
+int main (void)
 {
-    printf("Hello world!\n");
+    int ch;
+    int rows, cols;
+    printf("Enter a character and two integers:\n");
+    while ((ch = getchar()) != '\n')
+    {
+        if (scanf("%d%d", &rows, &cols) !=2) break; //讀不到兩個字元的話就跳出
+        display(ch, rows, cols);
+        printf("Enter another character and two integers;\n");
+        printf("Enter a newline to quit.\n");
+        while(getchar()!='\n');
+    }
+    printf("Bye.\n");
     return 0;
+}
+
+void display(char cr, int lines, int width)
+{
+    int row, col;
+    for(row = 1; row <=lines; row++)
+    {
+        for (col = 1; col <= width; col++)
+            putchar(cr);
+        putchar('\n');
+    }
 }
